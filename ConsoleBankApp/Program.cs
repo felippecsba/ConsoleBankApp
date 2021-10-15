463889abf0c6e4e61e635d3fdc4e6f64b0eea233
Felippe Araújo <felippecsbaraujo@gmail.com>
2021-10-14 21:10:21 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleBankApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleBankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
